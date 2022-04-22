06ba461755962bdc85678c71cee3ad01509479c6
Valentina <valentina.kramoreva@yandex.ru>
2022-04-22 12:44:47 +0400

    message

--- app.js
+++ app.js
@@ -12,19 +12,15 @@ app.post('/', async (req, res) => {
     const user = await userSchema.create({ name, avatar, about });
     // eslint-disable-next-line no-console
     console.log(req.body);
-    res.json(user);
+    res.status(200).json(user);
   } catch (e) {
-    res.status(500).json(e);
+    res.json(e);
   }
 });
 
 async function startApp() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mestodb', {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect('mongodb://localhost:27017/mestod');
     app.listen(PORT, () => {
       // eslint-disable-next-line no-console
       console.log(`Ссылка на сервер: http://localhost:${PORT}`);
