04d8e6eccf44c93a006b83c3caafd53906c7705f
Valentina <valentina.kramoreva@yandex.ru>
2022-04-24 20:35:54 +0400

    message

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const { getUsers, getUsersById } = require('../controllers/users');
+const { getUsers, getUsersById, createUser } = require('../controllers/users');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.post('./users', createUser)
-// router.get("/users/me", getMyInfo);
 
-// router.get("/users/:userId", celebrate({
-//   params: Joi.object().keys({
-//     userId: Joi.string().length(24).hex(),
-//   }),
-// }), getUserByID);
 
 module.exports = router;
