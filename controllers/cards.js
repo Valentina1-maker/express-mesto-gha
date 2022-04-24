336284e7e398a3d2a8cf577a0e695716d89dae6b
Valentina <valentina.kramoreva@yandex.ru>
2022-04-24 22:55:27 +0400

    message

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,7 +15,7 @@ module.exports.deleteCardById = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body
-  Card.create({ name, link})
+  Card.create({ name, link, owner: req.user._id})
     .then(card => res.send({ card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
