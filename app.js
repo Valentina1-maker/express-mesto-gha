7e1831ab0d4f2ac7aadda1ad2dfe61f94e9e7f61
Valentina <valentina.kramoreva@yandex.ru>
2022-04-25 21:11:52 +0400

    message

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use((req, res) => {
 })
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Ссылка на сервер:');
+  console.log(`Ссылка на сервер: http://localhost:${PORT}`);
 });
 
