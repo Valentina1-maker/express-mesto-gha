d9b5ae51e3b322c1c15f13629640c272a6a4f264
Valentina <valentina.kramoreva@yandex.ru>
2022-04-26 11:57:10 +0400

    message

--- controllers/users.js
+++ controllers/users.js
@@ -6,23 +6,24 @@ module.exports.getUsers = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.getUsersById = (req, res, next) => {
+module.exports.getUsersById = (req, res) => {
   User.findById(req.params.userId)
     .then(users => res.send({ data: users }))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      }
-    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, avatar, about } = req.body
   User.create({ name, avatar, about })
     .then(users => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+        return
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    })
 };
 
 module.exports.updateAvatar = (req, res) => {
@@ -30,7 +31,13 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id,
     { avatar })
     .then((user) => { res.status(200).send(user); })
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+        return
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    })
 };
 
 module.exports.updateProfile = (req, res) => {
