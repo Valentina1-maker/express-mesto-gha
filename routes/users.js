e818a22ef173e88bf11d11289149121d64686769
Valentina <valentina.kramoreva@yandex.ru>
2022-04-26 20:32:38 +0400

    message

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getUsers, getUsersById, createUser, updateProfile, updateAvatar } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUsersById);
+router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
 router.patch("/users/me",  updateProfile);
 router.patch("/users/me/avatar", updateAvatar)
