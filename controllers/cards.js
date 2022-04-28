8a51c1d96709934d4fc8e1c17c4a5e929ad67ae3
Valentina <valentina.kramoreva@yandex.ru>
2022-04-28 20:28:39 +0400

    message

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,32 +2,57 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then(cards => res.send( {data: cards} ))
+    .then(cards => res.send({ data: cards }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 
 module.exports.deleteCardById = (req, res) => {
-  Card.findById(req.params.cardId)
-    .then(card => res.send({ message: `Карточка c _id: ${req.params.cardId} успешно удалена.` }))
+  Card.findByIdAndDelete(req.params.cardId)
+    .then(() => res.send({ message: `Карточка c _id: ${req.params.cardId} успешно удалена.` }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.createCard = (req, res, next) => {
-  const { name, link, id: _id} = req.body
-  Card.create({ name, link, owner: _id})
+module.exports.createCard = (req, res) => {
+  const { name, link, id: _id } = req.body
+
+  Card.create({ name, link, owner: _id })
     .then(card => res.send({ card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: id._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-)
+module.exports.likeCard = (req, res) => {
+  const { userId } = req.body
+  const { cardId } = req.params
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: id._id } }, // убрать _id из массива
-  { new: true },
-)
+  Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: userId } }, // добавить _id в массив, если его там нет
+    { new: true },
+    function (err, card) {
+      if (err) {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      } else {
+        res.send(card)
+      }
+    }
+  )
+}
+
+module.exports.dislikeCard = (req, res) => {
+  const { userId } = req.body
+  const { cardId } = req.params
+
+  Card.findByIdAndUpdate(
+    cardId,
+    { $pull: { likes: userId } }, // добавить _id в массив, если его там нет
+    { new: true },
+    function (err, card) {
+      if (err) {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      } else {
+        res.send(card)
+      }
+    }
+  )
+}
