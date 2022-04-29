60573a798aecb0ebbc67676b4f0ad1faf1cbbfcb
Valentina <valentina.kramoreva@yandex.ru>
2022-04-29 19:02:03 +0400

    message

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use("/", require("./routes/users"));
-app.use("/", require("./routes/cards"));
 app.use((req, res, next) => {
   req.user = {
     _id: '62665b28d6891543a9192150'
@@ -20,6 +18,10 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use("/", require("./routes/users"));
+app.use("/", require("./routes/cards"));
+
 
 app.use((req, res) => {
   res.status(404).send({message: 'Страница не найдена'});
