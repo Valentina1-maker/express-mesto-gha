f6ffe5870d928072e21189bb07bdb35c4375435a
Valentina <valentina.kramoreva@yandex.ru>
2022-04-29 18:27:50 +0400

    message

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,15 +28,24 @@ module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     cardId,
     { $addToSet: { likes: userId } }, // добавить _id в массив, если его там нет
-    { new: true },
-    function (err, card) {
-      if (err) {
-        res.status(500).send({ message: 'Произошла ошибка' });
-      } else {
-        res.send(card)
-      }
-    }
+    { new: true, runValidators: true },
   )
+    .then((card) => {
+      if (card) {
+        res.status(200).send([card]);
+      } else {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.'});
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === "IncorrectID") {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.'});
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    })
 }
 
 module.exports.dislikeCard = (req, res) => {
@@ -46,13 +55,22 @@ module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     cardId,
     { $pull: { likes: userId } }, // добавить _id в массив, если его там нет
-    { new: true },
-    function (err, card) {
-      if (err) {
-        res.status(500).send({ message: 'Произошла ошибка' });
-      } else {
-        res.send(card)
-      }
-    }
+    { new: true, runValidators: true },
   )
+    .then((card) => {
+      if (card) {
+        res.status(200).send([card]);
+      } else {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.'});
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === "IncorrectID") {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.'});
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    })
 }
