1c5139afc1ae98eac8c3a21ab0e079e2aa6eeeea
Valentina <valentina.kramoreva@yandex.ru>
2022-05-12 22:53:40 +0400

    massage

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getMyInfo, getUserById, createUser, updateProfile, updateAvatar,
 } = require('../controllers/users');
+// eslint-disable-next-line import/no-unresolved
+const regExp = require('../regexp/regexp');
 
 router.get('/users', getUsers);
 router.get('/users/me', getMyInfo);
-router.get('/users/:userId', getUserById);
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex(),
+  }),
+}), getUserById);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateProfile);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(regExp),
+  }),
+}), updateAvatar);
+
 router.post('/users', createUser);
-router.patch('/users/me', updateProfile);
-router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
