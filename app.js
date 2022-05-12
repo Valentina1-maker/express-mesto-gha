aabf4d17ae454720eef8563668078f8995aad010
Valentina <valentina.kramoreva@yandex.ru>
2022-05-12 21:36:28 +0400

    massage

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
-const { celebrate, Joi } = require('celebrate');
 
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -15,20 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(8).max(35),
-  }),
-}), createUser);
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use(auth);
 
 app.use('/', require('./routes/users'));
