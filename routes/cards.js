be56fa79ff6d982eca898bdb2fef4b8d6f37a9d9
Valentina <valentina.kramoreva@yandex.ru>
2022-05-15 18:22:21 +0400

    check card reqest

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const regExp = require('../regExp/regExp');
+
 const {
   getCards, deleteCardById, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
 router.delete('/cards/:cardId', deleteCardById);
-router.post('/cards', createCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regExp),
+  }),
+}), createCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), likeCard);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
