d8acf55612f1636b08fabb7ca52c4c1c342b257e
Valentina <valentina.kramoreva@yandex.ru>
2022-05-20 20:05:02 +0400

    zdvgfdgdf

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -19,8 +19,8 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
+    res.end();
   }
 
-  return next();
+  next();
 };
